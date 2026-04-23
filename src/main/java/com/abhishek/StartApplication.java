0c31abc949c26d3e7c00fb8d97c1e5e3e7599c92
OmarChouchane <omar.ch52831@gmail.com>
2026-04-23 10:24:12 +0100

    Remove hardcoded password from model
    Removed the hardcoded password attribute from the controller (deleted the local password variable and model.addAttribute("password", password)) to avoid exposing credentials in the view. The compiled class was updated accordingly.

--- src/main/java/com/abhishek/StartApplication.java
+++ src/main/java/com/abhishek/StartApplication.java
@@ -14,8 +14,6 @@ public class StartApplication {
     public String index(final Model model) {
         model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
         model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
-        String password = "Admin123!";
-        model.addAttribute("password", password);
         return "index";
     }
 
